dff48ad59edc00ffb05c9465b3f43d5625ef8afb
Alex Delgado <alex.delgado@fowardslash.com>
2020-05-14 17:38:50 -0500

    Initial commit

---
+++ sample.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Plugin Name: Sample Plugin
+ * Plugin URI:
+ * Description: A sample WordPress plugin
+ * Author: Alex Delgado
+ * Author URI: https://alexdelgado.github.io/
+ * Version: 1.0.0
+ * License: GPL2+
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * @package sample
+ */
+
+define( 'SAMPLE_PLUGIN_URI', plugins_url( null, __FILE__ ) );
+define( 'SAMPLE_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
